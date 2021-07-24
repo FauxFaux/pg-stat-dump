fa96d7c3419590111ad2c7de4fbacbb0854c640b
Chris West (Faux) <git@goeswhere.com>
2021-07-24 12:23:28 +0100

    feat: loop

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-use std::convert::TryInto;
+use std::fs;
+use std::io::Write;
+use std::time::{Duration, Instant};
 
 use anyhow::{anyhow, Context, Result};
 use chrono::{DateTime, SecondsFormat, Utc};
 use lazy_static::lazy_static;
 use native_tls::TlsConnector;
 use postgres::types::Oid;
+use postgres::{Client, Statement};
 use postgres_native_tls::MakeTlsConnector;
 use regex::Regex;
-use std::fs;
-use std::io::Write;
+use zstd::Encoder;
 
 lazy_static! {
     static ref WS: Regex = Regex::new("\\s+").expect("static regex");
 }
 
-fn main() -> Result<()> {
+struct Pg {
+    client: Client,
+    stat: Statement,
+}
+
+fn connect() -> Result<Pg> {
     let connector = TlsConnector::builder()
         .danger_accept_invalid_certs(true)
         .danger_accept_invalid_hostnames(true)
-        .build()?;
+        .build()
+        .with_context(|| anyhow!("configuring tls connection"))?;
     let connector = MakeTlsConnector::new(connector);
 
     let mut client =
-        postgres::Client::connect("host=localhost user=postgres sslmode=require", connector)?;
+        postgres::Client::connect("host=localhost user=postgres sslmode=require", connector)
+            .with_context(|| anyhow!("connecting to database"))?;
+
+    // millis
+    client
+        .execute("set statement_timeout to 5000", &[])
+        .with_context(|| anyhow!("setting statement timeout"))?;
 
     let stat = client.prepare(
         concat!(
@@ -30,22 +44,21 @@ fn main() -> Result<()> {
             " from pg_stat_activity order by backend_start, pid"))
         .with_context(|| anyhow!("preparing select pg_stat_activity"))?;
 
-    let started_time = Utc::now();
-    let path = format!(
-        "stat-activity-{}.zst",
-        started_time.to_rfc3339_opts(SecondsFormat::Secs, true)
-    );
-    let mut output = zstd::Encoder::new(fs::File::create(path)?, 9)?.auto_finish();
+    Ok(Pg { client, stat })
+}
 
-    let columns = stat.columns();
+fn fetch(conn: &mut Pg) -> Result<Vec<Vec<String>>> {
+    let columns = conn.stat.columns();
     let headers: Vec<_> = columns.iter().map(|c| c.name().to_string()).collect();
-
-    let mut mins: Vec<usize> = headers.iter().map(|s| s.len()).collect();
 
     let mut lines = Vec::with_capacity(32);
     lines.push(headers);
 
-    for row in client.query(&stat, &[])? {
+    for row in conn
+        .client
+        .query(&conn.stat, &[])
+        .with_context(|| anyhow!("executing prepared query"))?
+    {
         let mut strings = Vec::with_capacity(columns.len());
         for (i, column) in columns.iter().enumerate() {
             strings.push(match column.type_().name() {
@@ -60,7 +73,11 @@ fn main() -> Result<()> {
         lines.push(strings);
     }
 
-    for line in &lines {
+    Ok(lines)
+}
+
+fn render(lines: &[Vec<String>], mins: &mut [usize]) -> String {
+    for line in lines {
         for (col, min) in line.iter().zip(mins.iter_mut()) {
             if col.len() > *min {
                 *min = col.len();
@@ -69,7 +86,7 @@ fn main() -> Result<()> {
     }
 
     let mut buf = String::with_capacity(lines.len() * 300);
-    for line in &lines {
+    for line in lines {
         let last = mins.len() - 1;
         for (col, min) in line.iter().zip(mins.iter()).take(last) {
             buf.push_str(&format!("{:1$}", col, min + 3));
@@ -78,8 +95,69 @@ fn main() -> Result<()> {
         buf.push('\n');
     }
 
-    output.write_all(buf.as_bytes())?;
-    output.flush()?;
+    buf
+}
+
+fn open() -> Result<Encoder<'static, fs::File>> {
+    let path = format!(
+        "stat-activity-{}.zst",
+        Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true)
+    );
+    Ok(zstd::Encoder::new(fs::File::create(path)?, 9)?)
+}
+
+fn attempt_close(conn: Pg) {
+    if conn.client.is_closed() {
+        return;
+    }
+
+    drop(conn.stat);
+
+    if let Err(e) = conn.client.close() {
+        eprintln!("{:?} error closing: {:?}", Utc::now(), e);
+    }
+}
+
+fn main() -> Result<()> {
+    let mut conn = connect()?;
+
+    let started_time = Instant::now();
+    let mut output = open()?;
+
+    let mut mins: Box<[usize]> = vec![0usize; conn.stat.columns().len()].into_boxed_slice();
+
+    loop {
+        let lines = match fetch(&mut conn) {
+            Ok(lines) => lines,
+            Err(e) => {
+                eprintln!("{:?} retrying error: {:?}", Utc::now(), e);
+                attempt_close(conn);
+                conn = connect().with_context(|| anyhow!("reconnecting after fetch error"))?;
+                fetch(&mut conn).with_context(|| anyhow!("fetch after reconnection"))?
+            }
+        };
+
+        let buf = render(&lines, &mut mins);
+
+        output
+            .write_all(buf.as_bytes())
+            .with_context(|| anyhow!("compressing / writing"))?;
+        output
+            .flush()
+            .with_context(|| anyhow!("flushing compressed data"))?;
+
+        if started_time.elapsed().gt(&Duration::from_secs(60 * 60)) {
+            break;
+        }
+        std::thread::sleep(Duration::from_secs(57));
+    }
+
+    attempt_close(conn);
+
+    output
+        .do_finish()
+        .with_context(|| anyhow!("finalising output file during clean exit"))?;
+
     Ok(())
 }
 
